6ac33d28d2743079576a76a750d595ae87ae62de
Josh Harvey <jwharve@g.clemson.edu>
2018-10-04 19:24:11 -0400

    updated arduino code for box_logic, still need real testing

--- brain/box_logic.ino
+++ brain/box_logic.ino
@@ -1,3 +1,4 @@
+/*
 class Box
 {
   public:
@@ -28,4 +29,36 @@ Box::goToUnit(int xInd, int yInd)
 {
   go(this->X0 + xInd * this->unitX, this->Y0 + yInd * this->unitY,z_range);
 }
+*/
 
+class Box
+{
+  public:
+    Box(int Xin, int Yin, int numXin, int numYin, int X0in, int Y0in);
+    int X, Y; // width (X) and length (Y) of the box [steps]
+    int numX, numY; // number of units in each direction [count]
+    int X0, Y0; // (X[zero] and Y[zero]) middle of unit (0,0) in the overall system  [steps]
+    
+    int goToUnit(int xInd, int yInd);
+    
+    float unitX, unitY; // width (x) and length (y) of each unit [steps]
+};
+
+Box::Box(int Xin, int Yin, int numXin, int numYin, int X0in, int Y0in)
+{
+  X = Xin;
+  Y = Yin;
+  numX = numXin;
+  numY = numYin;
+  X0 = X0in;
+  Y0 = Y0in;
+
+  unitX = (float)Xin / (float)numXin;
+  unitY = (float)Yin / (float)numYin;
+}
+
+int Box::goToUnit(int xInd, int yInd)
+{
+  go(X0 + xInd * unitX, Y0 + yInd * unitY, z_range);
+  return 1;
+}
