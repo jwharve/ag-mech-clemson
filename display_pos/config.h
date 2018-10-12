29edf07c889c2279c8ce5da68ec1cf12e10d1df4
Josh Harvey <jwharve@g.clemson.edu>
2018-10-12 16:29:30 -0400

    removed utilities

--- display_pos/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef BRAIN_H
-#define BRAIN_H
-
-#define SAFETY 20 /* how many steps away from edges are set to end bounds */
-
-#endif
--- losses/config.h
+++
@@ -1,2 +0,0 @@
-#define SAFETY 150 /* how many steps away from edges are set to end bounds */
-
