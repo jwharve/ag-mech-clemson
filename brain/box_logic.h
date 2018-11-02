1740f1b1703c3a3fc5e9d57fb9c9fde480137377
Josh Harvey <jwharve@g.clemson.edu>
2018-11-02 11:13:20 -0400

    added array for plant info

--- brain/box_logic.h
+++ brain/box_logic.h
@@ -2,6 +2,7 @@
 #define BOX_LOGIC_H
 
 #include "move.h"
+#include <stdio.h>
 
 #define SLIDE_STEPS 30
 
@@ -14,13 +15,18 @@ class Tool
     int offsetY = 0; // y offset of tool from middle of tool head
     int currentTool = EMPTY; // current tool
     int depth = 0; // depth of tool from bottom of tool head
-
+    
     // location of tool storage in X, Y, Z
     int toolLocX = 0; // UPDATE THESE VALUES
     int toolLocY = 0;
     int toolLocZ = 0;
     
     void pickup(int tool_num); // picks up a certain tool
+};
+
+struct plantInfo
+{
+  int height = 0;
 };
 
 class Box
@@ -30,10 +36,14 @@ class Box
     int X, Y; // width (X) and length (Y) of the box [steps]
     int numX, numY; // number of units in each direction [count]
     int X0, Y0; // (X[zero] and Y[zero]) middle of unit (0,0) in the overall system  [steps]
+
+    struct plantInfo ** plants;
     
     int goToUnit(int xInd, int yInd);
     
     float unitX, unitY; // width (x) and length (y) of each unit [steps]
+
+    ~Box();
 };
 
 extern volatile int z_range;
--- brain/box_logic.ino
+++ brain/box_logic.ino
@@ -2,6 +2,7 @@ Tool head;
 
 void Tool::pickup(int tool_num)
 {
+  // maybe read in from file
   switch (tool_num)
   {
     default:
@@ -36,8 +37,19 @@ Box::Box(int Xin, int Yin, int numXin, int numYin, int X0in, int Y0in)
   X0 = X0in;
   Y0 = Y0in;
 
-  unitX = (float)Xin/ (float)(numXin-1);
-  unitY = (float)Yin/ (float)(numYin-1);
+  unitX = (float)Xin/ (float)(numX-1);
+  unitY = (float)Yin/ (float)(numY-1);
+
+  plants = (struct plantInfo **)malloc(numX*sizeof(struct plantInfo *));
+  int i,j;
+  for (i = 0; i < numXin; i++)
+  {
+    plants[i] = (struct plantInfo *)malloc(numY*sizeof(struct plantInfo));
+    for (j = 0; j < numY; j++)
+    {
+      plants[i][j].height = 0;
+    }
+  }
 }
 
 int Box::goToUnit(int xInd, int yInd)
@@ -46,4 +58,14 @@ int Box::goToUnit(int xInd, int yInd)
   Serial.println(Y0 + yInd * unitY);
   go(X0 + xInd * unitX, Y0 + yInd * unitY, 0);
   return 1;
+}
+
+Box::~Box(void)
+{
+  int i;
+  for (i = 0; i < numX; i++)
+  {
+    free(plants[i]);
+  }
+  free(plants);
 }
