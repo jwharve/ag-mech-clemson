1c62a02f8099ec31322a12cd29cc5ef2ea1a727c
Joshua Harvey <jwharve@g.clemson.edu>
2019-02-01 17:55:22 -0500

    fixed return type

--- brain/init.cpp
+++ brain/init.cpp
@@ -56,7 +56,7 @@ This function calibrates the endpoints of the machine
 It provides a safety margin of SAFETY steps
 Goes from x-min to x-max, y-min to y-max, then z-min to z-max.
 */
-int calibrate()
+void calibrate()
 {
   int i;
 
