ffa0f5649750e8e62a721f5a996ce9e426dbdd1a
Joshua Harvey <jwharve@g.clemson.edu>
2019-03-08 12:22:11 -0500

    UPDATED PINS BACK TO ORIGINAL

--- brain/pins.h
+++ brain/pins.h
@@ -4,31 +4,31 @@
 /* SWAPPED X AND Z */
 /* SWAPPED Z_MIN AND Z_MAX */
 
-#define Z_STEP_PIN         54
-#define Z_DIR_PIN          55
-#define Z_ENABLE_PIN       56
 #define Z_MIN_PIN          14
 #define Z_MAX_PIN          19
 
-#define Y_STEP_PIN         46
-#define Y_DIR_PIN          48
-#define Y_ENABLE_PIN       38
 #define Y_MIN_PIN          18
 #define Y_MAX_PIN  		   15
 
-#define X_STEP_PIN         26
-#define X_DIR_PIN          28
-#define X_ENABLE_PIN       62
 #define X_MAX_PIN          2
 #define X_MIN_PIN          3
 
-#define E_STEP_PIN         60
-#define E_DIR_PIN          61
-#define E_ENABLE_PIN       24
 
-#define Q_STEP_PIN         36
-#define Q_DIR_PIN          34
-#define Q_ENABLE_PIN       30
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+
+#define E_STEP_PIN         26
+#define E_DIR_PIN          28
+#define E_ENABLE_PIN       24
 
 #define SDPOWER            -1
 #define SDSS               53
