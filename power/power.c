d534a43223b2ffd50e05f847f8bae98d3cf3313a
ag-mech-clemson <agmechclemson@gmail.com>
2019-03-22 15:05:57 -0400

    got power working

--- power/power.c
+++ power/power.c
@@ -1,23 +1,24 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define ON_PIN 28
+#define ON_PIN 29
 
 int main(int argc, char * argv[])
 {
 //	digitalWrite(ON_PIN,LOW);
 
 	wiringPiSetup();
+	pinMode(ON_PIN,OUTPUT);
 	digitalWrite(ON_PIN,LOW);
 
-	if (argc < 2)
+	if (argc != 2)
 	{
 		digitalWrite(ON_PIN,LOW);
 		printf("power off\n");
 		return 0;
 	}
 
-	if (argv[1][0] == 'o')
+	if (argv[1][0] == '1')
 	{
 		digitalWrite(ON_PIN,HIGH);
 		printf("power on\n");
